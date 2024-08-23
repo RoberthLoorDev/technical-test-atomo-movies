8a254d59b46f1299951596c30aecb85dcce04a21
RoberthLoorDev <dayvidlife@gmail.com>
2024-08-23 00:41:31 -0500

    add: template for consultations

---
+++ src/services/tmdbApi.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const API = axios.create({
+  baseURL: "https://api.themoviedb.org/3",
+  params: {
+    language: "en-US",
+  },
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${import.meta.env.VITE_TMDB_API_KEY_AUTHORIZATION}`,
+  },
+});
+
+export const getPopularMovies = (page = 1) => {
+  return API.get("/movie/popular", { params: { page } });
+};
+
+export const getMovieDetails = async (id = 533535) => {
+  return API.get(`/movie/${id}`);
+};
